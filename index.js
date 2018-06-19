9690b027f783b2c0b5d82dfae0c2f63fa1bdefeb
Adrian Navarrete <a.navarrete.uk@gmail.com>
2018-06-19 23:05:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // write your code below!
 
 var name = "Adrian"
+<<<<<<< HEAD
 var height = 74
+=======
+var height = "74"
+>>>>>>> 183705c86d9119a604eca5be637554c419661e51
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
+<<<<<<< HEAD
 console.log(height + 1)
+=======
+console.log(parseInt(height) + 1)
+>>>>>>> 183705c86d9119a604eca5be637554c419661e51
 
 
 
